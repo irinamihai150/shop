121d9bacb010ea5ba6ff89b1d4d48b29fc1b1f0c
irinamihai150 <irinamihai150@gmail.com>
2023-10-17 22:05:54 +0100

    adding product details

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -4,12 +4,12 @@ const dresses = [
 		name: "Elegant Purple Evening Gown",
 		image: "/images/image1.jpg",
 		description:
-			"A stunning red evening gown, perfect for special occasions and formal events. Features a flowing design and intricate embroidery.",
+			"A stunning purple evening gown, perfect for special occasions and formal events. Features a flowing design and intricate embroidery.",
 		brand: "Fashion Couture",
 		category: "Dresses",
 		price: 199.99,
 		countInStock: 15,
-		rating: 4.7,
+		rating: 3.7,
 		numReviews: 8,
 	},
 	{
@@ -20,10 +20,10 @@ const dresses = [
 			"A beautiful lace wedding dress that will make your special day even more memorable. This dress features intricate lace details and a flattering silhouette.",
 		brand: "Bridal Elegance",
 		category: "Dresses",
-		price: 899.99,
+		price: 599.99,
 		countInStock: 10,
 		rating: 4.9,
-		numReviews: 12,
+		numReviews: 4,
 	},
 	{
 		_id: "3",
@@ -40,14 +40,14 @@ const dresses = [
 	},
 	{
 		_id: "4",
-		name: "Little Black Dress",
+		name: "Little Red Dress",
 		image: "/images/image4.jpg",
 		description:
 			"A classic little black dress that belongs in every wardrobe. This versatile dress can be dressed up or down for various occasions.",
 		brand: "Chic Styles",
 		category: "Dresses",
 		price: 59.99,
-		countInStock: 25,
+		countInStock:0,
 		rating: 4.3,
 		numReviews: 15,
 	},
@@ -61,8 +61,8 @@ const dresses = [
 		category: "Dresses",
 		price: 39.99,
 		countInStock: 30,
-		rating: 4.6,
-		numReviews: 18,
+		rating: 2.6,
+		numReviews: 2,
 	},
 ]
 
