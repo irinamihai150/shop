ff14add04e7a00f5283c002d17e23264027a52da
irinamihai150 <irinamihai150@gmail.com>
2023-10-17 20:41:55 +0100

    add fake data

---
+++ frontend/src/products.js
@@ -0,0 +1,69 @@
+const dresses = [
+	{
+		_id: "1",
+		name: "Elegant Purple Evening Gown",
+		image: "/images/image1.jpg",
+		description:
+			"A stunning red evening gown, perfect for special occasions and formal events. Features a flowing design and intricate embroidery.",
+		brand: "Fashion Couture",
+		category: "Dresses",
+		price: 199.99,
+		countInStock: 15,
+		rating: 4.7,
+		numReviews: 8,
+	},
+	{
+		_id: "2",
+		name: "Lace Wedding Dress",
+		image: "/images/image2.jpg",
+		description:
+			"A beautiful lace wedding dress that will make your special day even more memorable. This dress features intricate lace details and a flattering silhouette.",
+		brand: "Bridal Elegance",
+		category: "Dresses",
+		price: 899.99,
+		countInStock: 10,
+		rating: 4.9,
+		numReviews: 12,
+	},
+	{
+		_id: "3",
+		name: "Bohemian Maxi Dress",
+		image: "/images/image3.jpg",
+		description:
+			"Get ready for summer with this bohemian-inspired maxi dress. It features a colorful print and a relaxed fit, making it perfect for beach days.",
+		brand: "Boho Chic",
+		category: "Dresses",
+		price: 69.99,
+		countInStock: 20,
+		rating: 4.5,
+		numReviews: 10,
+	},
+	{
+		_id: "4",
+		name: "Little Black Dress",
+		image: "/images/image4.jpg",
+		description:
+			"A classic little black dress that belongs in every wardrobe. This versatile dress can be dressed up or down for various occasions.",
+		brand: "Chic Styles",
+		category: "Dresses",
+		price: 59.99,
+		countInStock: 25,
+		rating: 4.3,
+		numReviews: 15,
+	},
+	{
+		_id: "5",
+		name: "Floral Summer Dress",
+		image: "/images/image5.jpg",
+		description:
+			"Enjoy the warmth of summer in this floral print dress. It features a flattering A-line silhouette and adjustable straps for comfort.",
+		brand: "Summer Vibes",
+		category: "Dresses",
+		price: 39.99,
+		countInStock: 30,
+		rating: 4.6,
+		numReviews: 18,
+	},
+]
+
+export default dresses
