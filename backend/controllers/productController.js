86fb6f4d4425f2454fb706084335fbc1eb14723a
irinamihai150 <irinamihai150@gmail.com>
2023-10-27 22:26:15 +0100

    add errorhandler

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+import Product from "../models/productModel.js"
+//desc fetch all products
+//route GET /api/products
+const getProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({})
+	res.json(products)
+})
+
+//desc fetch  product by id
+//route GET /api/products/:id
+
+const getProductById = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id)
+
+	if (product) {
+		return res.json(product)
+	} else {
+		res.status(404)
+		throw new Error("Resource not found")
+	}
+})
+
+export { getProducts, getProductById }
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 import cors from "cors"
 dotenv.config()
 import connectDB from "./config/db.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 const port = process.env.PORT || 5000
 
@@ -17,6 +18,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`)
 })
