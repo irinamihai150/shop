2ae60ce5d433e74a5363b3d1e121d42a4003f20c
irinamihai150 <irinamihai150@gmail.com>
2023-11-06 10:45:07 +0000

    fix

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+import User from "../models/userModel.js"
+
+//desc auth user and get token
+//route GET /api/users/login
+const authUser = asyncHandler(async (req, res) => {
+	res.send("auth user")
+})
+
+//desc register user
+//route POST /api/users
+const registerUser = asyncHandler(async (req, res) => {
+	res.send("register user")
+})
+
+//desc logout user/ clear cookie
+//route POST /api/users/logout
+const logoutUser = asyncHandler(async (req, res) => {
+	res.send("logout user")
+})
+
+//desc get user profile
+//route GET /api/users/profile
+const getUserProfile = asyncHandler(async (req, res) => {
+	res.send("user profile")
+})
+
+//desc update user profile
+//route PUT /api/users/profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+	res.send("update user profile")
+})
+
+//desc get users
+//route Get /api/users
+const getUsers = asyncHandler(async (req, res) => {
+	res.send("get users")
+})
+
+//desc get users by Id
+//route Get /api/users/:id
+const getUserById = asyncHandler(async (req, res) => {
+	res.send("get user by ID")
+})
+
+//desc delete users
+//route Delete /api/users/:id
+const deleteUser = asyncHandler(async (req, res) => {
+	res.send("delete user")
+})
+
+//desc update user
+//route PUT /api/users/:id
+const updateUser = asyncHandler(async (req, res) => {
+	res.send("update user")
+})
+
+export {
+	authUser,
+	registerUser,
+	logoutUser,
+	getUserProfile,
+	getUserById,
+	updateUserProfile,
+	getUsers,
+	updateUser,
+	deleteUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import dotenv from "dotenv"
 import cors from "cors"
 dotenv.config()
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
