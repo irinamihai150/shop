a718f3b2408ad68c618e6acdcddebc4a045303b3
irinamihai150 <irinamihai150@gmail.com>
2023-11-06 20:42:23 +0000

    add cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js"
 import User from "../models/userModel.js"
+import jwt from "jsonwebtoken"
 
 //desc auth user and get token
 //route GET /api/users/login
@@ -9,11 +10,22 @@ const authUser = asyncHandler(async (req, res) => {
 	const user = await User.findOne({ email })
 
 	if (user && (await user.matchPassword(password))) {
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "30d",
+		})
+
+		//set jwt as http only cookie
+		res.cookie("jwt", token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV !== "development",
+		})
 		res.json({
 			_id: user._id,
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
+			sameSite: "strict",
+			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 		})
 	} else {
 		res.status(401)
