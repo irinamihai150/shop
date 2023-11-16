5428733ed136af08ed88c02ca85bea7be2189b12
irinamihai150 <irinamihai150@gmail.com>
2023-11-16 21:57:41 +0000

    get my orders

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -29,6 +29,13 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
 			}),
 			keepUnusedDataFor: 5,
 		}),
+
+		getMyOrders: builder.query({
+			query: () => ({
+				url: `${ORDERS_URL}/mine`,
+			}),
+			keepUnusedDataFor: 5,
+		}),
 	}),
 })
 
@@ -37,4 +44,5 @@ export const {
 	useGetOrderDetailsQuery,
 	usePayOrderMutation,
 	useGetPayPalClientIdQuery,
+	useGetMyOrdersQuery,
 } = ordersApiSlice
