fdc9b2717c66060d24ead88131fe362c5d7bc5d8
irinamihai150 <irinamihai150@gmail.com>
2023-11-23 22:34:38 +0000

    implement search

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from "react-bootstrap"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 import Product from "../components/Product"
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 import Loader from "../components/Loader"
@@ -13,6 +13,11 @@ const HomeScreen = () => {
 	})
 	return (
 		<>
+			{keyword && (
+				<Link to='/' className='btn btn-light mb-4'>
+					Go Back
+				</Link>
+			)}
 			{isLoading ? (
 				<Loader />
 			) : error ? (
